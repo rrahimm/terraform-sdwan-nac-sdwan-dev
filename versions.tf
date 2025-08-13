cc6040720da3066f68f0344b8fecfb5126bbe247
rrahimm <39684933+rrahimm@users.noreply.github.com>
2025-08-13 15:31:50 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     sdwan = {
       source  = "CiscoDevNet/sdwan"
-      version = ">= 0.6.2"
+      version = "0.6.2"
     }
     utils = {
       source  = "netascode/utils"
